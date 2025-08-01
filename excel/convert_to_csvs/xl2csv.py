30c9f794075899769112f3901c9ad37004294be7
Takuya Suzuki <takuya.suzuki05@gmail.com>
2025-08-01 23:09:25 +0900

    add excel to csvs converter

---
+++ excel/convert_to_csvs/xl2csv.py
@@ -0,0 +1,44 @@
+import csv
+
+from pathlib import Path
+from typing import Union
+
+import openpyxl
+
+
+def excel2csv(infile: Union[str, Path], outdir: Union[str, Path] = '.'):
+    """Create one csv file for each sheet in the workbook.
+
+    Args:
+        infile (Union[str, Path]): Input Excel file
+        outdir (Union[str, Path], optional): Output directory. Defaults to '.'.
+
+    Raises:
+        ValueError: If input Excel file does not exist.
+        ValueError: If input file is not an Excel file.
+    """
+    infile = Path(infile)
+    if not infile.exists():
+        raise ValueError('"{}" does not exist.'.format(infile))
+    if infile.suffix != '.xlsx':
+        raise ValueError('"{}" is not a Excel file.'.format(infile))
+
+    wb = openpyxl.load_workbook(str(infile))
+
+    if not outdir.exists():
+        outdir.mkdir(parents=True)
+
+    for iws in wb.sheetnames:
+        ws = wb[iws]
+        fout = outdir / '{}.csv'.format(iws)
+        with open(str(fout), newline='', mode='w') as csvfile:
+            writer = csv.writer(csvfile)
+            for row in ws.rows:
+                writer.writerow([cell.value for cell in row])
+    wb.close()
+
+
+if __name__ == '__main__':
+    input = Path('iris-dataset.xlsx')
+    outputs = Path('out_example')
+    excel2csv(input, outputs)
